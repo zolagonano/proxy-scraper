f1e17038a4ad5e040ecfdcf09bc1c8719148adf7
Zola Gonano <zolagonano@protonmail.com>
2024-04-20 15:19:27 +0000

    Add Shadowsocks support

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -16,12 +16,25 @@ pub struct MTProxy {
     pub secret: String,
 }
 
+/// Represents a Shadowsocks proxy.
+#[derive(Debug)]
+pub struct Shadowsocks {
+    /// The host address of the Shadowsocks proxy.
+    pub host: String,
+    /// The port number for the Shadowsocks proxy.
+    pub port: u32,
+    /// The password associated with the Shadowsocks proxy.
+    pub password: String,
+    /// The encryption method used by the Shadowsocks proxy.
+    pub method: String,
+}
+
 /// A scraper for extracting MTProxy information from a given source string.
 pub struct Scraper();
 
 impl Scraper {
     fn seperate_links(text: &str) -> String {
-        let regex = Regex::new(r#"\bhttps?://[^\s<>"']+[^.,;!?)"'\s]"#).unwrap();
+        let regex = Regex::new(r#"\b(https|ss)?://[^\s<>"']+[^.,;!?)"'\s]"#).unwrap();
         let mut links = String::new();
         for cap in regex.captures_iter(text) {
             links.push_str(&cap[0].replace("&amp;amp;", "&").replace("%3D", "="));
@@ -43,7 +56,7 @@ impl Scraper {
     ///
     /// ```
     /// let source = "Hello https://t.me/proxy?server=proxy.example.com&port=8080&secret=mysecret";
-    /// let proxies = Scraper::scrape_mtproxy(source);
+    /// let proxies = proxy_scraper::Scraper::scrape_mtproxy(source);
     /// println!("{:?}", proxies);
     /// ```
     pub fn scrape_mtproxy(source: &str) -> Vec<MTProxy> {
@@ -86,6 +99,43 @@ impl Scraper {
             };
         }
 
+        proxy_list
+    }
+
+    // TODO: Needs Error Handling
+    pub fn scrape_shadowsocks(source: &str) -> Vec<Shadowsocks> {
+        let source = &Self::seperate_links(source);
+        let mut proxy_list: Vec<Shadowsocks> = Vec::new();
+        let regex = Regex::new(
+            r#"ss://((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?)@((.+):\d+)#"#,
+        )
+        .unwrap();
+
+        for captures in regex.captures_iter(source) {
+            let base64_part = captures.get(1).unwrap().as_str();
+            let host = captures.get(3).unwrap().as_str().to_string();
+            let port: u32 = captures
+                .get(4)
+                .unwrap()
+                .as_str()
+                .to_string()
+                .parse::<u32>()
+                .unwrap();
+
+            let decoded_base64_part =
+                String::from_utf8(base64::decode(&base64_part).unwrap()).unwrap();
+            let parts: Vec<&str> = decoded_base64_part.split(":").collect();
+
+            let method = parts[0].to_string();
+            let password = parts[1].to_string();
+
+            proxy_list.push(Shadowsocks {
+                host,
+                port,
+                password,
+                method,
+            });
+        }
         proxy_list
     }
 }
